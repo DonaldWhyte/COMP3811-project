407b401c0dcce990015e7253a52fb4dbe90e6e4d
Donald Whyte <donaldwhyte0@gmail.com>
2013-12-08 15:02:54 +0000

    Implemented efficient ray-AABB bounding box code

--- src/AABB.cpp
+++ src/AABB.cpp
@@ -12,6 +12,29 @@ AABB::AABB(const Vector3& boxMin, const Vector3& boxMax)
 
 bool AABB::intersects(const Ray& ray, float tMin, float tMax) const
 {
+    float intervalMin = tMin;
+    float intervalMax = tMax;
+    const Vector3& origin = ray.origin();
+    const Vector3& inverseDirection = ray.inverseDirection();
+
     int posNeg = ray.directionSigns[0];
-    float t0 = (bounds[posNeg].x - ray.origin().x) * ray.inverseDirection().x;
+    float t0 = (bounds[posNeg].x - origin.x) * inverseDirection.x;
+    float t1 = (bounds[1 - posNeg].x - origin.x) * inverseDirection.x;
+    if (t0 > intervalMin) intervalMin = t0;
+    if (t1 < intervalMax) intervalMax = t1;
+    if (intervalMin > intervalMax) return false;
+
+    posNeg = ray.directionSigns[1];
+    t0 = (bounds[posNeg].y - origin.y) * inverseDirection.y;
+    t1 = (bounds[1 - posNeg].y - origin.y) * inverseDirection.y;
+    if (t0 > intervalMin) intervalMin = t0;
+    if (t1 < intervalMax) intervalMax = t1;
+    if (intervalMin > intervalMax) return false;
+
+    posNeg = ray.directionSigns[2];
+    t0 = (bounds[posNeg].z - origin.z) * inverseDirection.z;
+    t1 = (bounds[1 - posNeg].z - origin.z) * inverseDirection.z;
+    if (t0 > intervalMin) intervalMin = t0;
+    if (t1 < intervalMax) intervalMax = t1;
+    return (intervalMin <= intervalMax);
 }
