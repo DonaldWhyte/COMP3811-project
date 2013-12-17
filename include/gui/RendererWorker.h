e76e59f454185589e63e9ca466ca19fef077e77e
Donald Whyte <sc10dw@cslin040.csunix.comp.leeds.ac.uk>
2013-12-17 10:54:36 +0000

    Made sampling method configurable in the renderer worker

--- include/gui/RendererWorker.h
+++ include/gui/RendererWorker.h
@@ -7,6 +7,13 @@
 
 namespace raytracer { namespace gui {
 
+enum SamplingMethod
+{
+	SINGLESAMPLING = 0,
+	UNIFORM_MULTISAMPLING,
+	RANDOM_MULTISAMPLING
+};
+
 class RendererWorker : public QObject
 {
 
@@ -14,6 +21,14 @@ class RendererWorker : public QObject
 
 public:
 	RendererWorker(Raytracer* renderer, Image* canvas);
+	
+	/* Getters/setters for sampling method. */
+	void setSamplingMethod(SamplingMethod newMethod);
+	void setNumSamples(unsigned int newNumSamples);
+	/* Retrieves information on sampling method. */
+	SamplingMethod getSamplingMethod() const;
+	unsigned int getNumSamples() const;
+	
 	
 public slots:
 	void render();
@@ -24,8 +39,20 @@ signals:
 	void error(QString error);
 	
 private:
+	typedef bool (RendererWorker::*PixelRenderingMethod)(int, int, unsigned int, unsigned int, Colour&);
+
+	bool renderSinglesamplePixel(int i, int j, unsigned int canvasWidth,
+		unsigned int canvasHeight, Colour& result);
+	bool renderUniformMultisamplePixel(int i, int j, unsigned int canvasWidth,
+		unsigned int canvasHeight, Colour& result);
+	bool renderRandomMultisamplePixel(int i, int j, unsigned int canvasWidth,
+		unsigned int canvasHeight, Colour& result);
+	
 	Raytracer* renderer;
 	Image* canvas;
+	// Determines the sampling method used and how many samples are tkaen	
+	SamplingMethod samplingMethod;
+	unsigned int numSamples;
 
 };
 
--- src/gui/RendererWorker.cpp
+++ src/gui/RendererWorker.cpp
@@ -6,8 +6,28 @@ using namespace gui;
 static const Colour BACKGROUND_COLOUR(0.2f, 0.2f, 0.2f);
 
 RendererWorker::RendererWorker(Raytracer* renderer, Image* canvas) :
-	renderer(renderer), canvas(canvas)
+	renderer(renderer), canvas(canvas), samplingMethod(SINGLESAMPLING), numSamples(3)
 {
+}
+
+void RendererWorker::setSamplingMethod(SamplingMethod newMethod)
+{
+	samplingMethod = newMethod;
+}
+
+void RendererWorker::setNumSamples(unsigned int newNumSamples)
+{
+	numSamples = newNumSamples;
+}
+
+SamplingMethod RendererWorker::getSamplingMethod() const
+{
+	return samplingMethod;
+}
+
+unsigned int RendererWorker::getNumSamples() const
+{
+	return numSamples;
 }
 	
 void RendererWorker::render()
@@ -17,15 +37,26 @@ void RendererWorker::render()
     unsigned int canvasHeight = canvas->getHeight();
     Colour resultantColour;
    
+	// By defualt, render single sampled pixels
+	PixelRenderingMethod renderingMethod = &RendererWorker::renderSinglesamplePixel;
+	// Pick rendering method to use based on chosen sampling method
+	switch (samplingMethod)
+	{
+	case UNIFORM_MULTISAMPLING:
+		renderingMethod = &RendererWorker::renderUniformMultisamplePixel;
+		break;
+	case RANDOM_MULTISAMPLING:
+		renderingMethod = &RendererWorker::renderUniformMultisamplePixel;
+		break;
+	}
+	
+	// Render using the chosen pixel rendering method
 	for (unsigned int j = 0; (j < canvasHeight); j++)
 	{
 		for (unsigned int i = 0; (i < canvasWidth); i++)
         {
-            // Convert pixel coordinates (i, j) to viewing plane coordinates (x, y)
-            // Note that this gets the pixel CENTRE
-            float x = (static_cast<float>(i) + 0.5f) / canvasWidth; // a
-            float y = (static_cast<float>(j) + 0.5f) / canvasHeight; // b
-            bool hit = renderer->raytrace(x, y, resultantColour);
+        	Colour resultantColour;
+        	bool hit = ((*this).*renderingMethod)(i, j, canvasWidth, canvasHeight, resultantColour);
             if (hit)
                 canvas->set(i, j, resultantColour);
             else
@@ -35,4 +66,35 @@ void RendererWorker::render()
     }
     
 	emit finished();    
+}
+
+bool RendererWorker::renderSinglesamplePixel(int i, int j,
+	unsigned int canvasWidth, unsigned int canvasHeight, Colour& result)
+{
+    // Convert pixel coordinates (i, j) to viewing plane coordinates (x, y)
+    // Note that this gets the pixel CENTRE due to 0.5f
+    float x = (static_cast<float>(i) + 0.5f) / canvasWidth; // a
+    float y = (static_cast<float>(j) + 0.5f) / canvasHeight; // b
+    return renderer->raytrace(x, y, result);
+}
+
+bool RendererWorker::renderUniformMultisamplePixel(int i, int j,
+	unsigned int canvasWidth, unsigned int canvasHeight, Colour& result)
+{
+	// Define RANGE the uniformly sampled pixels will be in
+    float minX = static_cast<float>(i) / canvasWidth;
+    float minY = static_cast<float>(j) / canvasHeight;
+    float maxX = static_cast<float>(i + 1) / canvasWidth;
+    float maxY = static_cast<float>(j + 1) / canvasHeight;
+    return renderer->uniformMultisample(minX, minY, maxX, maxY, numSamples, result);
+}
+
+bool RendererWorker::renderRandomMultisamplePixel(int i, int j,
+	unsigned int canvasWidth, unsigned int canvasHeight, Colour& result)
+{
+    float minX = static_cast<float>(i) / canvasWidth;
+    float minY = static_cast<float>(j) / canvasHeight;
+    float maxX = static_cast<float>(i + 1) / canvasWidth;
+    float maxY = static_cast<float>(j + 1) / canvasHeight;
+    return renderer->randomMultisample(minX, minY, maxX, maxY, numSamples, result);
 }
