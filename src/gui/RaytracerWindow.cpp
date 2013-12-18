a8ad9fb593a8fd8e77a0970649895066e02edf51
Donald Whyte <sc10dw@cslin077.csunix.comp.leeds.ac.uk>
2013-12-18 12:10:57 +0000

    Created all controls for toolbox:

--- src/gui/RaytracerWindow.cpp
+++ src/gui/RaytracerWindow.cpp
@@ -11,7 +11,7 @@ RaytracerWindow::RaytracerWindow(Raytracer* renderer) : renderer(renderer)
 {
 	// Set title and size of window
 	setWindowTitle("COMP3811 Graphics Project -- Raytracer (Donald Whyte)");
-	resize(800, 800);
+	resize(800, 600);
 	// Create menus
 	fileMenu = menuBar()->addMenu("&File");
 	saveAction = fileMenu->addAction("&Save");
@@ -22,16 +22,84 @@ RaytracerWindow::RaytracerWindow(Raytracer* renderer) : renderer(renderer)
 	
 	// Create each section of the toolbox
 	raytracerSettings = new QGroupBox("Raytracers");
+		sampMethodLabel = new QLabel("Sampling Method");
+		sampMethod = new QComboBox();
+		sampMethod->addItem("Single Sample");
+		sampMethod->addItem("Uniform Multisampling");
+		sampMethod->addItem("Random Multisampling");
+		rayRowOneLayout = new QHBoxLayout();		
+		rayRowOneLayout->addWidget(sampMethodLabel);
+		rayRowOneLayout->addWidget(sampMethod);
+		numSamplesLabel = new QLabel("# Samples");
+		numSamples = new QSpinBox();
+		numSamples->setRange(1, 10);
+		numSamples->setValue(1);
+		rayRowTwoLayout = new QHBoxLayout();
+		rayRowTwoLayout->addWidget(numSamplesLabel);
+		rayRowTwoLayout->addWidget(numSamples);
+		sizeLabel = new QLabel("Size");	
+		widthBox = new QSpinBox();
+		widthBox->setRange(50, 4000);
+		numSamples->setValue(500);
+		xLabel = new QLabel("x");
+		heightBox = new QSpinBox();
+		heightBox->setRange(50, 4000);
+		numSamples->setValue(500);
+		rayRowThreeLayout = new QHBoxLayout();	
+		rayRowThreeLayout->addWidget(sizeLabel);
+		rayRowThreeLayout->addWidget(widthBox);
+		rayRowThreeLayout->addWidget(xLabel);
+		rayRowThreeLayout->addWidget(heightBox);
+		raytracerSettingsLayout = new QVBoxLayout();
+		raytracerSettingsLayout->addLayout(rayRowOneLayout);
+		raytracerSettingsLayout->addLayout(rayRowTwoLayout);
+		raytracerSettingsLayout->addLayout(rayRowThreeLayout);		
+		raytracerSettings->setLayout(raytracerSettingsLayout);
 	effectsSettings = new QGroupBox("Effects");
+		localIlluminationSwitch = new QCheckBox("Local Illumination");
+		reflectRefractSwitch = new QCheckBox("Reflection/Refraction");
+		shadowsSwitch = new QCheckBox("Shadows");
+		effectsSettingsLayout = new QVBoxLayout();
+		effectsSettingsLayout->addWidget(localIlluminationSwitch);
+		effectsSettingsLayout->addWidget(reflectRefractSwitch);
+		effectsSettingsLayout->addWidget(shadowsSwitch);
+		effectsSettings->setLayout(effectsSettingsLayout);
 	sceneSettings = new QGroupBox("Scene");
+		terrainLabel = new QLabel("Terrain Heightmap Size");
+		terrainHeightmap = new QComboBox();
+		terrainHeightmap->addItem("Small");
+		terrainHeightmap->addItem("Medium");
+		terrainHeightmap->addItem("Large");
+		sceneRowOneLayout = new QHBoxLayout();
+		sceneRowOneLayout->addWidget(terrainLabel);
+		sceneRowOneLayout->addWidget(terrainHeightmap);		
+		viewpointLabel = new QLabel("Viewpoint");
+		viewpoint = new QComboBox();
+		viewpoint->addItem("One");
+		viewpoint->addItem("Two");
+		viewpoint->addItem("Three");		
+		sceneRowTwoLayout = new QHBoxLayout();
+		sceneRowTwoLayout->addWidget(viewpointLabel);
+		sceneRowTwoLayout->addWidget(viewpoint);			
+		sceneSettingsLayout = new QVBoxLayout();
+		sceneSettingsLayout->addLayout(sceneRowOneLayout);
+		sceneSettingsLayout->addLayout(sceneRowTwoLayout);
+		sceneSettings->setLayout(sceneSettingsLayout);
 	geometricOptSettings = new QGroupBox("Geometric Optimisation");
-	
+		useOctree = new QCheckBox("Enable Octree");
+		showOctree = new QCheckBox("Show Octree");
+		geometricOptSettingsLayout = new QVBoxLayout();
+		geometricOptSettingsLayout->addWidget(useOctree);
+		geometricOptSettingsLayout->addWidget(showOctree);
+		geometricOptSettings->setLayout(geometricOptSettingsLayout);
+	renderButton = new QPushButton("RENDER");
 	// Create actual toolbox widget to store all settings
 	toolboxLayout = new QVBoxLayout();
 	toolboxLayout->addWidget(raytracerSettings);
 	toolboxLayout->addWidget(effectsSettings);
 	toolboxLayout->addWidget(sceneSettings);
 	toolboxLayout->addWidget(geometricOptSettings);	
+	toolboxLayout->addWidget(renderButton);	
 	// Create toolbox dock to the right of the canvas
 	toolboxDock = new QDockWidget("Settings");
 	toolboxWidget = new QWidget();
@@ -51,6 +119,7 @@ RaytracerWindow::~RaytracerWindow()
 	delete renderButton;
 	delete showOctree;
 	delete useOctree;
+	delete geometricOptSettingsLayout;
 	delete geometricOptSettings;
 	delete viewpoint;
 	delete viewpointLabel;
@@ -58,10 +127,12 @@ RaytracerWindow::~RaytracerWindow()
 	delete terrainHeightmap;
 	delete terrainLabel;
 	delete sceneRowOneLayout;
+	delete sceneSettingsLayout;
 	delete sceneSettings;
 	delete shadowsSwitch;
 	delete reflectRefractSwitch;
 	delete localIlluminationSwitch;
+	delete effectsSettingsLayout;
 	delete effectsSettings;
 	delete heightBox;
 	delete xLabel;
@@ -74,6 +145,7 @@ RaytracerWindow::~RaytracerWindow()
 	delete sampMethod;
 	delete sampMethodLabel;
 	delete rayRowOneLayout;
+	delete raytracerSettingsLayout;
 	delete raytracerSettings;
 	delete toolboxLayout;
 	delete toolboxWidget;
